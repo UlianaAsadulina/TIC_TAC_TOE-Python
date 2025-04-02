08f4999e8a690af7c32af0934435d5c04c809f88
UlianaAsadulina <u.asadulina@icloud.com>
2025-04-02 10:40:28 -0400

    function for user move

--- app.py
+++ app.py
@@ -39,19 +39,46 @@ def display_board(board):
 display_board(board)
 
 
-# initialize list of free fields
 list_free=[]
 
 def make_list_of_free_fields(board):
     # The function browses the board and builds a list of all the free squares; 
     # the list consists of tuples, while each tuple is a pair of row and column numbers.
+    # initialize list of free fields
     for i in range(3):
         for j in range(3):
-            my_tuple = (i, j,)
-            list_free.append(my_tuple)
+            if type(board[i][j]) == int:
+                my_tuple = (i, j,)
+                list_free.append(my_tuple)
+    return list_free                
 
         
 make_list_of_free_fields(board) 
-print(list_free)
 
 
+currentPlayer = 'X'
+board[1][1] = "X"
+del_index = list_free.index((1,1))
+del list_free[del_index]
+display_board(board)
+
+print(make_list_of_free_fields(board))
+currentPlayer = 'O'
+
+def enter_move(board):
+    # The function accepts the board's current status, asks the user about their move, 
+    # checks the input, and updates the board according to the user's decision.
+
+    player_num = int(input("Enter your move: "))
+
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == player_num:
+                print(i, j)
+                board[i][j] = "O"
+                my_tuple = (i, j,)
+                index_tuple = list_free.index(my_tuple)
+                print(index_tuple)
+                del list_free[index_tuple]
+    print (board)
+    print(list_free)
